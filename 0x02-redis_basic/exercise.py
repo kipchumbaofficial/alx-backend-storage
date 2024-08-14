bccb2afe77b14dc179ded1d34beef9b34a5b72c6
Kipchumba Victor <victorkipchumba039@gmail.com>
2024-08-14 23:01:25 +0300

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 '''
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -23,3 +23,32 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float]:
+        '''get method:
+            Gets the original type of the value of a key
+        '''
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_int(self, key: str) -> Optional[int]:
+        '''get_int method:
+            Retrieves an integer
+        '''
+        value = self.get(key, fn=int)
+        if isinstance(value, int):
+            return value
+        return None
+
+    def get_str(self, key: str) -> Optional[str]:
+        '''get_str method:
+            Retrieves a String from redis
+        '''
+        value = self.get(key, fn=lambda x: x.decode('utf-8'))
+        if isinstance(value, str):
+            return value
+        return None
